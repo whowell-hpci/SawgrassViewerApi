66abc051f83aed679cbf9cb61682ab03c9bf4b30
William Howell <whowell@heritagepci.com>
2020-03-23 16:33:20 -0400

    aaa

--- SawgrassViewerApi/Startup.cs
+++ SawgrassViewerApi/Startup.cs
@@ -62,7 +62,7 @@ namespace SawgrassViewerApi
             {
                 app.UseDeveloperExceptionPage();
             }
-            app.UseCors(c => c.WithOrigins("http://vhpci-can.hpci.local", "http://10.100.60.65:91").AllowAnyHeader().AllowAnyMethod());
+            app.UseCors(c => c.WithOrigins("http://vhpci-can.hpci.local", "http://10.100.60.65").AllowAnyHeader().AllowAnyMethod());
             app.UseRouting();
             
             app.UseAuthentication();
